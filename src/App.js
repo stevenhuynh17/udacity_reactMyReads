723e809b54594b2c4f54eba51a500aa651cdc4dd
Steven <stevenhuynh17@gmail.com>
2018-04-29 20:18:32 -0700

    refactor: currentlyReading data coming from state

--- src/App.js
+++ src/App.js
@@ -11,9 +11,47 @@ import Search from './Search.jsx'
 
 class BooksApp extends React.Component {
   state = {
-    currentlyReading: [],
-    wantToRead: [],
-    read: []
+    currentlyReading: [
+      {
+        backgroundImage: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee"
+      },
+      {
+        backgroundImage: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
+        title: "Ender's Game",
+        author: "Orson Scott Card"
+      }
+    ],
+    wantToRead: [
+      {
+        backgroundImage: "",
+        title: "",
+        author: ""
+      },
+      {
+        backgroundImage: "",
+        title: "",
+        author: ""
+      }
+    ],
+    read: [
+      {
+        backgroundImage: "",
+        title: "",
+        author: ""
+      },
+      {
+        backgroundImage: "",
+        title: "",
+        author: ""
+      },
+      {
+        backgroundImage: "",
+        title: "",
+        author: ""
+      }
+    ]
   }
 
   componentDidMount() {
@@ -31,7 +69,7 @@ class BooksApp extends React.Component {
             <Header />
             <div className="list-books-content">
               <div>
-                <CurrentlyReading />
+                <CurrentlyReading books={this.state.currentlyReading}/>
                 <WantToRead />
                 <Read />
               </div>
