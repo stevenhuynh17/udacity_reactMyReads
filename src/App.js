260b5483dfa32bba5fcf38eac546f5fcacd2fba2
Steven <stevenhuynh17@gmail.com>
2018-04-29 19:40:42 -0700

    feat: routing functioning

--- src/App.js
+++ src/App.js
@@ -1,38 +1,52 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
+import { Route, Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import CurrentlyReading from './CurrentlyReading.jsx'
 import WantToRead from './WantToRead.jsx'
 import Read from './Read.jsx'
 import Header from './Header.jsx'
+import Search from './Search.jsx'
 
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
+    currentlyReading: [],
+    wantToRead: [],
+    read: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then((data) => {
+        console.log(data)
+      })
   }
 
   render() {
     return (
       <div className="app">
-        <div className="list-books">
-          <div className="list-books-content">
-            <div>
-              <CurrentlyReading />
-              <WantToRead />
-              <Read />
+        <Route exact path="/" render={() => (
+          <div className="list-books">
+            <Header />
+            <div className="list-books-content">
+              <div>
+                <CurrentlyReading />
+                <WantToRead />
+                <Read />
+              </div>
+            </div>
+            <div className="open-search">
+              <Link
+                to="/search">
+                Add a Book
+              </Link>
             </div>
           </div>
-          <div className="open-search">
-            <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-          </div>
-        </div>
+        )}/>
+        <Route path="/search" render={() => (
+          <Search />
+        )}/>
       </div>
     )}
 }
