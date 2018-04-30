8d1fb0368d3b83c00ceb9dac8f89b365d7cc34cd
Steven <stevenhuynh17@gmail.com>
2018-04-29 20:22:33 -0700

    refactor: want to read data coming from state

--- src/App.js
+++ src/App.js
@@ -25,14 +25,14 @@ class BooksApp extends React.Component {
     ],
     wantToRead: [
       {
-        backgroundImage: "",
-        title: "",
-        author: ""
+        backgroundImage: "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
+        title: "1776",
+        author: "David McCullough"
       },
       {
-        backgroundImage: "",
-        title: "",
-        author: ""
+        backgroundImage: "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api",
+        title: "Harry Potter and the Sorcerer's Stone",
+        author: "J.K. Rowling"
       }
     ],
     read: [
@@ -70,7 +70,7 @@ class BooksApp extends React.Component {
             <div className="list-books-content">
               <div>
                 <CurrentlyReading books={this.state.currentlyReading}/>
-                <WantToRead />
+                <WantToRead books={this.state.wantToRead}/>
                 <Read />
               </div>
             </div>
--- src/WantToRead.jsx
+++ src/WantToRead.jsx
@@ -3,25 +3,22 @@ import Book from './Book.jsx'
 
 class WantToRead extends Component {
   render() {
+    const { books } = this.props
+
     return(
       <div className="bookshelf">
         <h2 className="bookshelf-title">Want to Read</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <li>
-              <Book
-                backgroundImage="http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api"
-                title="1776"
-                author="David McCullough"
-              />
-            </li>
-            <li>
-              <Book
-                backgroundImage="http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api"
-                title="Harry Potter and the Sorcerer's Stone"
-                author="J.K. Rowling"
-              />
-            </li>
+            {books.map((book) => (
+              <li key={book.title}>
+                <Book
+                  backgroundImage={book.backgroundImage}
+                  title={book.title}
+                  author={book.author}
+                />
+              </li>
+            ))}
           </ol>
         </div>
       </div>
